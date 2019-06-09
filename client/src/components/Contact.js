b0380db9de6eeaa658702486e60b13585171e11b
Abdulmonem Nafea <abdulmonemamin@hotmail.com>
2019-06-10 03:48:14 +1000

    add a nodemailer to send email

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,6 +3,37 @@ import Title from './Title'
 
 export default class Contact extends Component {
 
+    state = {
+        firstName: '',
+        lastName: '',
+        email:'',
+        message:''
+    }
+
+    handlePostRequest = async(e) => {
+            e.preventDefault()
+            const { firstName, lastName, email, message} = this.state
+            const response = await fetch('/api/email', {
+                method: 'POST', 
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }, 
+                body: JSON.stringify({
+                    firstName, 
+                    lastName, 
+                    email, 
+                    message
+                })
+            })
+        }
+    
+    handleChange = e => (
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    )    
+
     render() {
         return (
             <div className='section m-0 mt-5 p-0' id='contact'>
@@ -14,22 +45,22 @@ export default class Contact extends Component {
                             logos
                         </div>
                         <div className="col-md-6 center">
-                            <form action="https://formspree.io/aamnafea@hotmail.com" method="POST">
+                            <form onSubmit={this.handlePostRequest}>
                                 <div className='form-group'>
                                     <label>Name</label>
                                     <div className="row">
                                         <div className="col">
-                                            <input type="text" className="form-control" name="first_name" placeholder="First name" />
+                                            <input type="text" className="form-control" name="firstName" placeholder="First name" onChange={this.handleChange}/>
                                         </div>
                                         <div className="col">
-                                            <input type="text" className="form-control" name="last_name" placeholder="Last name" />
+                                            <input type="text" className="form-control" name="lastName" placeholder="Last name" onChange={this.handleChange}/>
                                         </div>
                                     </div>
                                     <label className='mt-1'>Email address</label>
-                                    <input type="email" className="form-control" name="email" placeholder="name@example.com" />
+                                    <input type="email" className="form-control" name="email" placeholder="name@example.com" onChange={this.handleChange}/>
                                     <label className='mt-1'>Message</label>
-                                    <textarea className="form-control" name="message" rows="4"></textarea>
-                                    <button type="submit" class="btn btn-outline-secondary my-2" value="Send">Send</button>
+                                    <textarea className="form-control" name="message" rows="4" onChange={this.handleChange}></textarea>
+                                    <button type="submit" className="btn btn-outline-secondary my-2" value="Send">Send</button>
                                 </div>
                             </form>
                         </div>
---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodeMailer = require('nodemailer')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/api/hello', (req, res) => {
+    res.send('Hello from express')
+})
+
+app.post('/api/email', (req, res) => {
+    console.log(req.body)
+    let transporter = nodeMailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.email,
+            pass: process.env.password
+        }
+    })
+
+    let mailOptions ={
+        from: `aamnafea@gmail.com`, // sender address (who sends)
+        to: 'aamnafea@hotmail.com', // list of receivers (who receives)
+        subject: 'Portfolio Message', // Subject line
+        text: `${req.body.message}`,
+        html: `
+        <h3>first name: ${req.body.firstName}</h3>
+        
+        <h3>last name: ${req.body.lastName}</h3>
+        
+        <h3>e-mail: ${req.body.email}</h3>
+        
+        <p>${req.body.message}</p>`
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err){
+            return console.log(err)
+        }
+        
+        return console.log('message sent')
+    })
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`listening on port ${port}`))
