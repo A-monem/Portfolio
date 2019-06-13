eebcef2cbd51b69a2d5e8b812dc73c986774c61e
Abdulmonem Nafea <abdulmonemamin@hotmail.com>
2019-06-14 03:54:44 +1000

    add node server to package.json

---
+++ client/build/precache-manifest.089acdfce4059ca503ce25ef1a310e3c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41076f78392cf5b21afd528c4c04c751",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4670cc5be2dff5a95dec",
+    "url": "./static/css/main.662fe486.chunk.css"
+  },
+  {
+    "revision": "f83088a3533a7fa89819",
+    "url": "./static/js/2.435ae13f.chunk.js"
+  },
+  {
+    "revision": "4670cc5be2dff5a95dec",
+    "url": "./static/js/main.f7c20dcd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "fd77e7c4246148404fabbfc0e375c3ea",
+    "url": "./static/media/Abdelmoneim.fd77e7c4.jpg"
+  },
+  {
+    "revision": "fb70dddf53f4c32097845af091296c2c",
+    "url": "./static/media/basketball.fb70dddf.PNG"
+  },
+  {
+    "revision": "c1ee775dbdfe25d7d139c9d1a11f0f4c",
+    "url": "./static/media/blog.c1ee775d.png"
+  },
+  {
+    "revision": "e3852db5f4235724cfe404dd6f554bd9",
+    "url": "./static/media/gift.e3852db5.jpg"
+  },
+  {
+    "revision": "f5ff5e4722a82a4c3b779bdd86c95d92",
+    "url": "./static/media/header.f5ff5e47.jpeg"
+  },
+  {
+    "revision": "c1bbcf2489a42a52cf328b5203f9b209",
+    "url": "./static/media/resume.c1bbcf24.pdf"
+  },
+  {
+    "revision": "02a0066b989a5e3843435f8db6305584",
+    "url": "./static/media/trianglify1.02a0066b.png"
+  },
+  {
+    "revision": "88586d1deef8f8870499214ba238369f",
+    "url": "./static/media/war.88586d1d.png"
+  },
+  {
+    "revision": "be5d32eac480a9df70af60ba3f5c0dea",
+    "url": "./static/media/weather.be5d32ea.JPG"
+  },
+  {
+    "revision": "548038ab65574766b7dce685ff78311c",
+    "url": "./static/media/yelpcamp.548038ab.jpg"
+  }
+]);
